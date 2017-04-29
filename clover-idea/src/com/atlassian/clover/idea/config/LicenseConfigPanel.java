df1f4804de1d9bcf8b58f82d46b3910ac2e63894
Marek Parfianowicz <marek.parfianowicz@gmail.com>
2017-04-29 10:15:53 +0200

    OC-16: show 'free license' instead of 'evaluation license'

--- clover-idea/src/com/atlassian/clover/idea/config/LicenseConfigPanel.java
+++ clover-idea/src/com/atlassian/clover/idea/config/LicenseConfigPanel.java
@@ -2,12 +2,9 @@ package com.atlassian.clover.idea.config;
 
 import com.atlassian.clover.CloverLicense;
 import com.atlassian.clover.CloverLicenseDecoder;
-import com.atlassian.clover.LicenseDecoderException;
-import com.atlassian.clover.idea.CloverPlugin;
 import com.atlassian.clover.idea.util.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory;
 import com.intellij.openapi.vfs.VirtualFile;
-import com_atlassian_clover.CloverVersionInfo;
 
 import javax.swing.BorderFactory;
 import javax.swing.JLabel;
@@ -20,10 +17,6 @@ import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 public class LicenseConfigPanel extends JPanel implements ActionListener {
     private JPanel summaryPanel;
@@ -99,86 +92,18 @@ public class LicenseConfigPanel extends JPanel implements ActionListener {
         }
     }
 
-    private static final long MAINT_SLACK = 7L * 24L * 60L * 60L * 1000L; // from CloverStartup
-
-    private String lastCheckedLicenseText;
-
     private void verifyLicense() {
-        lastCheckedLicenseText = licenseTextArea.getText();
-
-        if (lastCheckedLicenseText.trim().length() == 0) {
-            handleEmptyLicense();
-            return;
-        }
-        CloverLicense license;
         try {
-            license = CloverLicenseDecoder.decode(lastCheckedLicenseText);
-        } catch (LicenseDecoderException e) {
-            licenseStatusLabel.setText("<html><body><font color=\"red\">Invalid</font></body></html>");
-            licenseMessageLabel.setText("Please paste a valid license.");
-            licenseTypeLabel.setText("");
-            return;
+            final CloverLicense license = CloverLicenseDecoder.decode("");
+            final String status = "<html><body><font color=\"green\">Licensed</font></body></html>";
+            licenseStatusLabel.setText(status);
+            licenseTypeLabel.setText(license.getLicenseName());
+            licenseMessageLabel.setText(license.getOwnerStatement() != null ? license.getOwnerStatement() : "-");
         } catch (Exception e) {
             licenseStatusLabel.setText("<html><body><font color=\"red\">Invalid</font></body></html>");
-            licenseMessageLabel.setText(e.getLocalizedMessage());
-            licenseTypeLabel.setText("");
-            return;
-        }
-
-        final String status;
-
-        if (license.isExpired()) {
-            status = "Your license has expired";
-        } else {
-            SimpleDateFormat df = new SimpleDateFormat("MMMM dd yyyy", Locale.US);
-            long buildDate;
-            try {
-                buildDate = df.parse(CloverVersionInfo.BUILD_DATE).getTime();
-            } catch (ParseException e) {
-                throw new RuntimeException("Internal Error: could not determine build date.");
-            }
-            if (license.maintenanceExpires() && buildDate > (license.getMaintenanceExpiryDate() + MAINT_SLACK)) {
-                status = "<html>Clover upgrades for your license ended "
-                        + df.format(new Date(license.getMaintenanceExpiryDate()))
-                        + ", and this version of Clover was built " + CloverVersionInfo.BUILD_DATE;
-            } else {
-                final String preExpiry = processPreExpiryStmt(license);
-                status = (preExpiry.trim().length() != 0) ? preExpiry : "<html><body><font color=\"green\">Licensed</font></body></html>";
-            }
-        }
-
-        licenseStatusLabel.setText(status);
-        licenseTypeLabel.setText(license.getLicenseName());
-        licenseMessageLabel.setText(license.getOwnerStatement() != null ? license.getOwnerStatement() : "-");
-    }
-
-    private void handleEmptyLicense() {
-        final long installDate = CloverPlugin.getPlugin().getInstallDate();
-        final long now = System.currentTimeMillis();
-        final long millisLeft = installDate + CloverPlugin.EVALUATION_PERIOD - now;
-
-        if (millisLeft < 0) {
-            licenseStatusLabel.setText("<html><body><font color=\"red\">Evaluation license expired");
-            licenseMessageLabel.setText("<html>Please generate new evaluation license using the link below if you wish to continue your evaluation.");
-        } else {
-            long daysLeft = millisLeft / CloverLicense.ONE_DAY;
-            licenseStatusLabel.setText("You have " + daysLeft + " day(s) before your evaluation license expires.");
             licenseMessageLabel.setText("");
+            licenseTypeLabel.setText("");
         }
-
-        licenseTypeLabel.setText("Clover Evaluation License");
-    }
-
-    private static String processPreExpiryStmt(CloverLicense license) {
-        String preExpiryStmt = license.getPreExpiryStatement();
-        if (preExpiryStmt.contains("$daysleft")) {
-            long daysLeft = license.getDaysTillExpiry();
-            if (daysLeft < 0) {
-                daysLeft = 0;
-            }
-            preExpiryStmt = preExpiryStmt.replace("$daysleft", String.valueOf(daysLeft));
-        }
-        return preExpiryStmt;
     }
 
     private boolean loadLicenseFile() {
