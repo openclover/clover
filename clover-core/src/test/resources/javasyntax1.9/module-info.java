3c55deaf2202ba8bab122a0271586f2a7fa70517
Marek Parfianowicz <marek.parfianowicz@gmail.com>
2018-08-07 23:54:27 +0200

    OC-61: more nasty test cases

--- clover-core/src/test/resources/javasyntax1.9/module-info.java
+++ clover-core/src/test/resources/javasyntax1.9/module-info.java
@@ -1,15 +1,20 @@
 import java9.internal.Internal;
 import java9.spi.SPI;
 
+// test: we must be able to parse expressions in annotations
 @SuppressWarnings("abc" + "def" + (2 * 23))
 module javasyntax {
+    // test: parse all possible keywords
     requires java.base;
     requires transitive java.xml;
     requires static java.sql;
     exports java9;
     exports java9.spi;
-    exports java9.internal to javasyntax.internal, javasyntax.internal2;
     opens java9.api to common.spi, common.api;
     uses java.lang.String;
     provides SPI with Internal;
+
+    // test: distinguish between keywords and identifiers
+    exports exports;
+    exports java9.internal to to, requires, transitive, exports, opens, uses, provides, with, java9.internal;
 }
