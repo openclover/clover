1b1022308b4796769804d9e0bdeb8c08e280440f
Marek Parfianowicz <marek.parfianowicz@gmail.com>
2018-08-12 01:20:08 +0200

    OC-61: CloverEnvTask no longer adds clover.jar to system classloader (JDK9 is more restrictive regarding classpath manipulation, let's get rid of this hack); removed obsolete CloverWarningTask; removed unused code from ClassPathUtil

--- clover-ant/src/main/java/com/atlassian/clover/ant/tasks/CloverEnvTask.java
+++ clover-ant/src/main/java/com/atlassian/clover/ant/tasks/CloverEnvTask.java
@@ -1,79 +1,17 @@
 package com.atlassian.clover.ant.tasks;
 
 import com.atlassian.clover.PrematureLibraryLoader;
-import com.atlassian.clover.api.CloverException;
-import org.openclover.util.ClassPathUtil;
 import com_atlassian_clover.CloverVersionInfo;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Diagnostics;
-import org.apache.tools.ant.Project;
 import org.apache.tools.ant.ProjectHelper;
 import org.apache.tools.ant.Task;
+import org.openclover.util.ClassPathUtil;
 
 import java.net.URL;
-import java.net.URLClassLoader;
 
-/**
- * This task must be loaded before all other
- * clover tasks. It ensures that clover is on
- * the System classpath, setting it if not.
- */
+
 public class CloverEnvTask extends Task {
-    /**
-     * The Clover compiler adapter class
-     */
-    public static final String CLOVER_ADAPTER = "com.atlassian.clover.ant.taskdefs.CloverCompilerAdapter";
-
-    static {
-        // check that Clover can be loaded correctly and extend classpath if not
-        try {
-            final String cloverJarPath = ClassPathUtil.getCloverJarPath();
-
-            if (cloverJarPath != null) {
-                try {
-                    URLClassLoader loader = ClassPathUtil.findSystemClassLoader(Project.class);
-                    if (!ClassPathUtil.isClassOnClassPath(CLOVER_ADAPTER, loader)) {
-                        ClassPathUtil.extendClassPath(cloverJarPath, loader);
-                        logDebug("Added '" + cloverJarPath + "' to the System Classloader '" + loader + "'");
-                        ClassPathUtil.assertOnClassPath(CLOVER_ADAPTER, loader);
-                    } else {
-                        logDebug(cloverJarPath + " is already on the System Classloader.");
-                    }
-
-                } catch (Throwable e) {
-                    logDebug("Error adding Clover '" + cloverJarPath +
-                            "' to the System Classloader: Cause " + e.getMessage(), e);
-                }
-            } else {
-                logDebug("Unable to determine the Clover jar path. Not extending System Classloader.");
-            }
-        } catch (Throwable t) {
-            logClassPathMessage();
-            logDebug(t.getMessage(), t);
-        }
-    }
-
-    private static void logClassPathMessage() {
-        System.out.println(
-                "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n" +
-                "Error determining the correct path to clover.jar. \n" +
-                "Ensure there is not an older copy of clover.jar on Ant's classpath. \n" +
-                "NB: Clover no longer requires clover.jar to be placed in Ant's lib directory.\n" +
-                "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    }
-
-    private static void logDebug(String message) {
-        logDebug(message, null);
-    }
-
-    private static void logDebug(String message, Throwable t) {
-        if (isDebug()) {
-            System.out.println(message);
-            if (t != null) {
-                t.printStackTrace(System.err);
-            }
-        }
-    }
 
     @Override
     public void init() throws BuildException {
@@ -85,7 +23,6 @@ public class CloverEnvTask extends Task {
      * Execute should never be called, in case it does, this
      * task will merely log the Clover version being used, and some environment
      * information.
-     * @throws BuildException
      */
     @Override
     public void execute() throws BuildException {
@@ -93,16 +30,11 @@ public class CloverEnvTask extends Task {
 
         ProjectHelper helper = (ProjectHelper) getProject().getReference(ProjectHelper.PROJECTHELPER_REFERENCE);
         try {
-            URLClassLoader loader = ClassPathUtil.findSystemClassLoader(Project.class);
-
-            URL importURL = loader.getResource("clover.xml");
+            URL importURL = CloverEnvTask.class.getResource("clover.xml");
             log("Loading clover.xml: " + importURL);
             helper.parse(getProject(), importURL);
         } catch (BuildException ex) {
             throw ProjectHelper.addLocationToBuildException(ex, getLocation());
-        } catch (CloverException e) {
-            log("Error importing clover.xml into project.");
-            throw new BuildException(e.getMessage(), e);
         } catch (Throwable e) {
             log("An unexpected error occurred while trying to import the bundled clover build file.");
             throw new BuildException(e.getMessage(), e);
--- clover-core/src/main/java/org/openclover/util/ClassPathUtil.java
+++ clover-core/src/main/java/org/openclover/util/ClassPathUtil.java
@@ -1,92 +1,18 @@
 package org.openclover.util;
 
-import com.atlassian.clover.api.CloverException;
 import com.atlassian.clover.util.FileUtils;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.net.URLClassLoader;
-import java.io.File;
 import java.security.CodeSource;
 
 /**
  * A Util class used for manipulating and querying the System Class path.
  */
 public class ClassPathUtil {
-
-    private static void addURL(@NotNull URLClassLoader ucl, @NotNull URL path)
-            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, CloverException {
-        try {
-            Method addURLMethod = URLClassLoader.class.getDeclaredMethod("addURL", new Class[]{URL.class});
-            addURLMethod.setAccessible(true);
-            addURLMethod.invoke(ucl, new Object[]{path});
-        } catch (NoSuchMethodException e) {
-            // should not happen
-            throw new CloverException("Unable to setup classpath extender", e);
-        }
-    }
-
-    /**
-     * extends the system classpath
-     * @param path the path to add
-     * @param loader the class loader to extend
-     * @throws CloverException if there is a problem extending the class path
-     */
-    public static void extendClassPath(@NotNull String path, @NotNull URLClassLoader loader) throws CloverException {
-
-        try {
-            URL url = new File(path).toURI().toURL();
-            addURL(loader, url);
-        } catch (Exception e) {
-            throw new CloverException("Unable to add paths to the ClassLoader", e);
-        }
-    }
-
-    /**
-     * Returns the first URLClassLoader found in the class loader ancestry.
-     * @param obj an Object to find the system class loader for
-     * @return first URLClassLoader found in the class loader ancestry
-     * @throws CloverException if no URLClassLoader can be found
-     */
-    @NotNull
-    public static URLClassLoader findSystemClassLoader(@NotNull Class obj) throws CloverException {
-        ClassLoader loader = obj.getClassLoader();
-        while (!(loader instanceof URLClassLoader)) {            
-            loader = loader.getParent();
-            if (loader == null) {
-                throw new CloverException("Unable to find a URLClassLoader for object: " + obj);
-            }
-        }
-        return (URLClassLoader) loader;
-    }
-
-    /**
-     * Determines whether the given className can be loaded from
-     * the System Class Loader.
-     * @see #findSystemClassLoader(Class) 
-     * @param className the name of the class to test
-     * @param loader the class loader to check
-     * @return <code>true</code> if the class is on the system classpath, <code>false</code> otherwise
-     */
-    public static boolean isClassOnClassPath(@NotNull String className, @NotNull ClassLoader loader) {
-        try {
-            Class.forName(className, true, loader);
-            return true;
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
-    }
-
-    public static void assertOnClassPath(@NotNull String className, @NotNull ClassLoader loader) throws CloverException {
-        if (!isClassOnClassPath(className, loader)) {
-            throw new CloverException(className + " is not on classpath of class loader: " + loader);
-        }
-    }
 
     /**
      * Returns full path to a Clover JAR file or <code>null</code> if it was unable to determine.
