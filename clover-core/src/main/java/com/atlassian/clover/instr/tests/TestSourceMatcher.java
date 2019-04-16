78709c5815a8b3503943f8ee7e4dcc04b0ceb844
Marek Parfianowicz <marek.parfianowicz@gmail.com>
2019-04-17 00:03:26 +0200

    OC-84: cleanup

--- clover-core/src/main/java/com/atlassian/clover/instr/tests/TestSourceMatcher.java
+++ clover-core/src/main/java/com/atlassian/clover/instr/tests/TestSourceMatcher.java
@@ -1,14 +1,13 @@
 package com.atlassian.clover.instr.tests;
 
-
 import java.io.File;
 import java.io.Serializable;
 
 
 public interface TestSourceMatcher extends Serializable {
 
-    public boolean matchesFile(File f);
+    boolean matchesFile(File f);
 
-    public TestDetector getDetector();
+    TestDetector getDetector();
 
 }
