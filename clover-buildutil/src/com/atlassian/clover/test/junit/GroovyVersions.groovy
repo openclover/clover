fece3b2506ce1db26692724d272738c92cce5d65
Marek Parfianowicz <marek.parfianowicz@gmail.com>
2019-07-23 22:53:17 +0200

    NONE: build process optimization - make <get> less chatty, use Ant 1.9.14 as default one (we cannot use Ant 1.10 or higher as it requires JDK8), reduce number of Ant and Spock versions being downloaded (the spock 0.7-groovy-2.0 is kept because it's compatible with groovy 2.x.x, while spock 1.0 is compatible with groovy 2.0-2.2 only), cleanup code in Ant/Groovy/SpockVersions classes

--- clover-buildutil/src/com/atlassian/clover/test/junit/GroovyVersions.groovy
+++ clover-buildutil/src/com/atlassian/clover/test/junit/GroovyVersions.groovy
@@ -1,18 +1,18 @@
 package com.atlassian.clover.test.junit
 
-public class GroovyVersions {
+class GroovyVersions {
     static String DEFAULT_VERSION = "2.4.15";
 
-    static Closure SPLIT = {
-        it.equals(null) ? null : it.split(",")
+    static Closure SPLIT = { String it ->
+        it == null ? null : it.split(",")
     }
 
-    static Closure CHOOSE_DEFAULT_SUPPORTED_IF_NULL_ELSE_SPLIT = {
-        it.equals(null) ? [GroovyVersions.DEFAULT_VERSION] : it.split(",")
+    static Closure CHOOSE_DEFAULT_SUPPORTED_IF_NULL_ELSE_SPLIT = { String it ->
+        it == null ? [DEFAULT_VERSION] : it.split(",")
     }
 
     /** Return hardcoded list of latest major versions if input string is <code>null</code>, otherwise split the string */
-    static Closure CHOOSE_LATEST_MAJOR_IF_NULL_ELSE_SPLIT = {
-        it.equals(null) ? ["1.6.9", "1.7.11", "1.8.9", "2.0.8", "2.1.9", "2.2.2", "2.3.11", "2.4.15"] : it.split(",")
+    static Closure CHOOSE_LATEST_MAJOR_IF_NULL_ELSE_SPLIT = { String it ->
+        it == null ? ["1.6.9", "1.7.11", "1.8.9", "2.0.8", "2.1.9", "2.2.2", "2.3.11", "2.4.15"] : it.split(",")
     }
 }
