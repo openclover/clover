aae06421dd576441d46707f54bce34f99895f988
Marek Parfianowicz <marek.parfianowicz@gmail.com>
2019-07-23 22:53:17 +0200

    NONE: build process optimization - make <get> less chatty, use Ant 1.9.14 as default one (we cannot use Ant 1.10 or higher as it requires JDK8), reduce number of Ant and Spock versions being downloaded (the spock 0.7-groovy-2.0 is kept because it's compatible with groovy 2.x.x, while spock 1.0 is compatible with groovy 2.0-2.2 only), cleanup code in Ant/Groovy/SpockVersions classes

--- clover-buildutil/src/com/atlassian/clover/test/junit/AntVersions.groovy
+++ clover-buildutil/src/com/atlassian/clover/test/junit/AntVersions.groovy
@@ -1,8 +1,13 @@
 package com.atlassian.clover.test.junit
 
-public class AntVersions {
-  static String DEFAULT_VERSION = "1.9.4";
+class AntVersions {
+    static String DEFAULT_VERSION = "1.9.14";
 
-  static Closure SPLIT = { it.equals(null) ? null : it.split(",") }
-  static Closure CHOOSE_DEFAULT_SUPPORTED_IF_NULL_ELSE_SPLIT = { it.equals(null) ? [AntVersions.DEFAULT_VERSION] : it.split(",") }
+    static Closure SPLIT = { String it ->
+        it == null ? null : it.split(",")
+    }
+
+    static Closure CHOOSE_DEFAULT_SUPPORTED_IF_NULL_ELSE_SPLIT = { String it ->
+        it == null ? [DEFAULT_VERSION] : it.split(",")
+    }
 }
