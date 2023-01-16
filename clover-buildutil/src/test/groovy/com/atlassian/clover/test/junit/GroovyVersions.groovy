8842f2e1c8a9373f47d927d381cda9689adb77b9
Marek Parfianowicz <marek.parfianowicz@gmail.com>
2023-01-16 22:47:05 +0100

    OC-116: drop groovy 1.x in matrix tests

--- clover-buildutil/src/test/groovy/com/atlassian/clover/test/junit/GroovyVersions.groovy
+++ clover-buildutil/src/test/groovy/com/atlassian/clover/test/junit/GroovyVersions.groovy
@@ -13,6 +13,6 @@ class GroovyVersions {
 
     /** Return hardcoded list of latest major versions if input string is <code>null</code>, otherwise split the string */
     static Closure CHOOSE_LATEST_MAJOR_IF_NULL_ELSE_SPLIT = { String it ->
-        it == null ? ["1.6.9", "1.7.11", "1.8.9", "2.0.8", "2.1.9", "2.2.2", "2.3.11", "2.4.21"] : it.split(",")
+        it == null ? ["2.0.8", "2.1.9", "2.2.2", "2.3.11", "2.4.21"] : it.split(",")
     }
 }
