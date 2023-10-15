0a7b678deddd632564a132d536657c5beac4ae74
Marek Parfianowicz <marek.parfianowicz@gmail.com>
2023-10-15 02:02:45 +0200

    OC-209: rewrite PrecannedClassLoader to @CompileStatic to workaround a problem with the super keyword

--- clover-core/src/test/groovy/com/atlassian/clover/util/PrecannedClassLoader.groovy
+++ clover-core/src/test/groovy/com/atlassian/clover/util/PrecannedClassLoader.groovy
@@ -1,19 +1,21 @@
 package com.atlassian.clover.util
 
-import static org.openclover.util.Maps.newHashMap
+import groovy.transform.CompileStatic
 
+@CompileStatic
 class PrecannedClassLoader extends ClassLoader {
-    private Map precanned = newHashMap()
+    private Map<String, byte[]> precanned = new HashMap<String, byte[]>()
 
-    PrecannedClassLoader(ClassLoader classLoader, Map precanned) {
+    PrecannedClassLoader(ClassLoader classLoader, Map<String, byte[]> precanned) {
         super(classLoader)
         this.precanned.putAll(precanned)
     }
 
+    @Override
     Class loadClass(String name, boolean resolve) throws ClassNotFoundException {
         Class result = null
         try {
-            result = findClassInternal(name)
+            result = findClass(name)
             if (result != null && resolve) {
                 resolveClass(result)
             }
@@ -31,12 +33,13 @@ class PrecannedClassLoader extends ClassLoader {
         return result
     }
 
-    protected Class findClassInternal(String name) throws ClassNotFoundException {
+    @Override
+    protected Class findClass(String name) throws ClassNotFoundException {
         if (precanned.containsKey(name)) {
             byte[] bytes = (byte[]) precanned.get(name)
             return defineClass(name, bytes, 0, bytes.length)
         } else {
-            return findClass(name)
+            return super.findClass(name)
         }
     }
 }
