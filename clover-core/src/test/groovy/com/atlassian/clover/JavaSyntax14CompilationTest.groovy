956e6b1bcce3c3fd9c22248251ce8ab2ee6396fe
Marek Parfianowicz <marek.parfianowicz@gmail.com>
2024-01-19 00:41:34 +0100

    OC-126: fix tests

--- clover-core/src/test/groovy/com/atlassian/clover/JavaSyntax14CompilationTest.groovy
+++ clover-core/src/test/groovy/com/atlassian/clover/JavaSyntax14CompilationTest.groovy
@@ -1,12 +1,15 @@
 package com.atlassian.clover
 
 import com.atlassian.clover.util.JavaEnvUtils
+import org.apache.tools.ant.BuildException
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 
 import static java.util.regex.Pattern.quote
+import static org.hamcrest.MatcherAssert.assertThat
+import static org.hamcrest.Matchers.isA
 import static org.junit.Assert.assertEquals
+import static org.junit.Assert.fail
 import static org.junit.Assume.assumeTrue
 
 /**
@@ -27,18 +30,20 @@ class JavaSyntax14CompilationTest extends JavaSyntaxCompilationTestBase {
     }
 
     @Test
-    void switchExpressionWithCaseWithColonCanUseYield() {
+    void switchStatementWithCaseWithColonCannotUseYield() {
         assumeTrue(JavaEnvUtils.isAtLeastJavaVersion(JavaEnvUtils.JAVA_14))
 
-        final String fileName = "Java14SwitchExpressionWithCaseColon.java"
-        instrumentAndCompileSourceFile(srcDir, mGenSrcDir, fileName, JavaEnvUtils.JAVA_14)
-
-        // using yield in switch expressions is allowed also for "case X:" form
-        assertFileMatches(fileName, quote("case 0:") + "\\w*" + R_INC + quote("yield 10;"))
-        assertFileMatches(fileName, quote("default:") + "\\w*" + R_INC + quote("yield 11;"))
-        assertFileMatches(fileName, quote("case 10:") + "\\w*" + R_INC + quote("yield 20;"))
-        assertFileMatches(fileName, quote("case 11:") + "\\w*" + R_INC + quote("yield 21;"))
-        assertFileMatches(fileName, quote("default:") + "\\w*" + R_INC + quote("yield 22;"))
+        final String fileName = "Java14SwitchStatementWithCaseColonFailed.java"
+        final File srcFile = new File(srcDir, fileName)
+        try {
+            // it's not allowed to use yield in switch statements (a value returned cannot be ignored)
+            // OpenClover grammar parser is simplified and allows 'yield' in place where any statement is allowed
+            // but the javac catches this
+            instrumentAndCompileSourceFile(srcDir, mGenSrcDir, fileName, JavaEnvUtils.JAVA_14)
+            fail("Compilation should have failed")
+        } catch (Exception ex) {
+            assertThat(ex, isA(BuildException.class))
+        }
     }
 
     @Test
@@ -47,11 +52,44 @@ class JavaSyntax14CompilationTest extends JavaSyntaxCompilationTestBase {
 
         final String fileName = "Java14SwitchExpressionWithCaseColonFailed.java"
         final File srcFile = new File(srcDir, fileName)
+        try {
+            // using break in switch expressions is NOT allowed for "case X:" form
+            // a reason is that a switch expression must return a value and break returns no value
+            // OpenClover is not so restrictive and instrumentation succeeds but javac catches this
+            instrumentAndCompileSourceFile(srcDir, mGenSrcDir, fileName, JavaEnvUtils.JAVA_14)
+            fail("Compilation should have failed")
+        } catch (Exception ex) {
+            assertThat(ex, isA(BuildException.class))
+        }
+    }
+
+    @Test
+    void testSwitchCaseWithColonsMixedWithExpressions() {
+        assumeTrue(JavaEnvUtils.isAtLeastJavaVersion(JavaEnvUtils.JAVA_14))
+
+        final String fileName = "Java14SwitchExpressionMixedCasesFailed.java"
+        final File srcFile = new File(srcDir, fileName)
         int returnCode = instrumentSourceFileNoAssert(srcFile, JavaEnvUtils.JAVA_14, [] as String[])
 
-        // using break in switch expressions is NOT allowed for "case X:" form
-        // a reason is that a switch expression must return a value and break returns no value
+        // it's not allowed to mix "case X:" with "case X ->" in the same switch statement
+        // OpenClover's parser should catch this
         assertEquals(1, returnCode)
+    }
+
+    @Test
+    void switchExpressionWithCaseWithColonCanUseYield() {
+        assumeTrue(JavaEnvUtils.isAtLeastJavaVersion(JavaEnvUtils.JAVA_14))
+
+        final String fileName = "Java14SwitchExpressionWithCaseColon.java"
+        instrumentAndCompileSourceFile(srcDir, mGenSrcDir, fileName, JavaEnvUtils.JAVA_14)
+
+        // using yield in switch expressions is allowed also for "case X:" form
+        // notice there are two R_INC - one for entering the case, one for the statement
+        assertFileMatches(fileName, quote("case 0:") + "\\s*" + R_INC + "\\s*" + R_INC + quote("yield 10;"))
+        assertFileMatches(fileName, quote("default:") + "\\s*" + R_INC + "\\s*" + R_INC + quote("yield 11;"))
+        assertFileMatches(fileName, quote("case 10:") + "\\s*" + R_INC + "\\s*" + R_INC + quote("yield 20;"))
+        assertFileMatches(fileName, quote("case 11:") + "\\s*" + R_INC + "\\s*" + R_INC + quote("yield 21;"))
+        assertFileMatches(fileName, quote("default:") + "\\s*" + R_INC + "\\s*" + R_INC + quote("yield 22;"))
     }
 
     @Test
@@ -66,18 +104,6 @@ class JavaSyntax14CompilationTest extends JavaSyntaxCompilationTestBase {
         assertFileMatches(fileName, quote("default:") + ".*" + R_INC + quote("break;"))
     }
 
-    @Ignore("Current grammar rule is too loose, and allows use of 'yield' in place where any statement is allowed")
-    @Test
-    void switchStatementWithCaseWithColonCannotUseYield() {
-        assumeTrue(JavaEnvUtils.isAtLeastJavaVersion(JavaEnvUtils.JAVA_14))
-
-        final String fileName = "Java14SwitchStatementWithCaseColonFailed.java"
-        final File srcFile = new File(srcDir, fileName)
-        int returnCode = instrumentSourceFileNoAssert(srcFile, JavaEnvUtils.JAVA_14, [] as String[])
-
-        // it's not allowed to use yield in switch statements (a value returned cannot be ignored)
-        assertEquals(1, returnCode)
-    }
 
     @Test
     void switchExpressionWithCaseAndDefaultCanUseLambdasReturningValues() {
@@ -87,12 +113,12 @@ class JavaSyntax14CompilationTest extends JavaSyntaxCompilationTestBase {
         instrumentAndCompileSourceFile(srcDir, mGenSrcDir, fileName, JavaEnvUtils.JAVA_14)
 
         // switch expression with value-returning lambdas
-        assertFileMatches(fileName, quote("case R -> ") + R_CASE_EXPRESSION_LEFT +  quote("\"red\";") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("case G -> ") + R_CASE_EXPRESSION_LEFT +  quote("\"green\";") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("case B -> ") + R_CASE_EXPRESSION_LEFT +  quote("\"blue\";") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("case 0 -> ") + R_CASE_EXPRESSION_LEFT +  quote("EvenOrOdd.EVEN;") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("case 1 -> ") + R_CASE_EXPRESSION_LEFT +  quote("EvenOrOdd.ODD;") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("default -> ") + R_CASE_EXPRESSION_LEFT +  quote("EvenOrOdd.UNKNOWN;") + R_CASE_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, quote("case R -> ") + R_CASE_EXPRESSION_LEFT + quote("\"red\";") + R_CASE_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, quote("case G -> ") + R_CASE_EXPRESSION_LEFT + quote("\"green\";") + R_CASE_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, quote("case B -> ") + R_CASE_EXPRESSION_LEFT + quote("\"blue\";") + R_CASE_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, quote("case 0 -> ") + R_CASE_EXPRESSION_LEFT + quote("EvenOrOdd.EVEN;") + R_CASE_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, quote("case 1 -> ") + R_CASE_EXPRESSION_LEFT + quote("EvenOrOdd.ODD;") + R_CASE_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, quote("default -> ") + R_CASE_EXPRESSION_LEFT + quote("EvenOrOdd.UNKNOWN;") + R_CASE_EXPRESSION_RIGHT)
     }
 
     @Test
@@ -103,17 +129,17 @@ class JavaSyntax14CompilationTest extends JavaSyntaxCompilationTestBase {
         instrumentAndCompileSourceFile(srcDir, mGenSrcDir, fileName, JavaEnvUtils.JAVA_14)
 
         // switch expression with void lambdas
-        assertFileMatches(fileName, quote("case R -> ") + R_CASE_EXPRESSION_LEFT_VOID +
+        assertFileMatches(fileName, quote("case R -> ") + R_CASE_EXPRESSION_LEFT +
                 quote("System.out.println(\"red\");") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("case G -> ") + R_CASE_EXPRESSION_LEFT_VOID +
+        assertFileMatches(fileName, quote("case G -> ") + R_CASE_EXPRESSION_LEFT +
                 quote("System.out.println(\"green\");") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("case B -> ") + R_CASE_EXPRESSION_LEFT_VOID +
+        assertFileMatches(fileName, quote("case B -> ") + R_CASE_EXPRESSION_LEFT +
                 quote("System.out.println(\"blue\");") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("case 0 -> ") + R_CASE_EXPRESSION_LEFT_VOID +
+        assertFileMatches(fileName, quote("case 0 -> ") + R_CASE_EXPRESSION_LEFT +
                 quote("System.out.println(EvenOrOdd.EVEN);") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("case 1 -> ") + R_CASE_EXPRESSION_LEFT_VOID +
+        assertFileMatches(fileName, quote("case 1 -> ") + R_CASE_EXPRESSION_LEFT +
                 quote("System.out.println(EvenOrOdd.ODD);") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("default -> ") + R_CASE_EXPRESSION_LEFT_VOID +
+        assertFileMatches(fileName, quote("default -> ") + R_CASE_EXPRESSION_LEFT +
                 quote("System.out.println(EvenOrOdd.UNKNOWN);") + R_CASE_EXPRESSION_RIGHT)
     }
 
@@ -124,17 +150,17 @@ class JavaSyntax14CompilationTest extends JavaSyntaxCompilationTestBase {
         final String fileName = "Java14SwitchExpressionCaseAndDefaultWithThrows.java"
         instrumentAndCompileSourceFile(srcDir, mGenSrcDir, fileName, JavaEnvUtils.JAVA_14)
 
-        assertFileMatches(fileName, quote("case -1 -> ") + R_CASE_EXPRESSION_LEFT +
-                quote("throw new IllegalArgumentException(\"negative\");") + R_CASE_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, quote("case -1 -> ") + R_CASE_THROW_EXPRESSION_LEFT +
+                quote("throw new IllegalArgumentException(\"negative\");") + R_CASE_THROW_EXPRESSION_RIGHT)
         assertFileMatches(fileName, quote("default -> ") + R_CASE_EXPRESSION_LEFT +
                 quote("0;") + R_CASE_EXPRESSION_RIGHT)
 
-        assertFileMatches(fileName, quote("case -1 -> ") + R_CASE_EXPRESSION_LEFT +
-                quote("throw new IllegalArgumentException(\"negative\");") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("case 0 -> ") + R_CASE_EXPRESSION_LEFT +
-                quote("throw new IllegalArgumentException(\"zero\");") + R_CASE_EXPRESSION_RIGHT)
-        assertFileMatches(fileName, quote("default -> ") + R_CASE_EXPRESSION_LEFT +
-                quote("throw new IllegalArgumentException(\"anything\");") + R_CASE_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, quote("case -1 -> ") + R_CASE_THROW_EXPRESSION_LEFT +
+                quote("throw new IllegalArgumentException(\"negative\");") + R_CASE_THROW_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, quote("case 0 -> ") + R_CASE_THROW_EXPRESSION_LEFT +
+                quote("throw new IllegalArgumentException(\"zero\");") + R_CASE_THROW_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, quote("default -> ") + R_CASE_THROW_EXPRESSION_LEFT +
+                quote("throw new IllegalArgumentException(\"anything\");") + R_CASE_THROW_EXPRESSION_RIGHT)
     }
 
     @Test
@@ -221,28 +247,15 @@ class JavaSyntax14CompilationTest extends JavaSyntaxCompilationTestBase {
         instrumentAndCompileSourceFile(srcDir, mGenSrcDir, fileName, JavaEnvUtils.JAVA_14)
 
         // a standalone statement in method
-        assertFileMatches(fileName, quote("") + R_CASE_EXPRESSION_LEFT +
-                quote("") + R_CASE_EXPRESSION_RIGHT)
+        assertFileMatches(fileName, R_INC + quote("switch (kk) {") + "\\s+" +
+                quote("case 77 ->") + R_CASE_EXPRESSION_LEFT +
+                quote(" System.out.println(\"77\")") + R_CASE_EXPRESSION_RIGHT + ";")
 
         // instance initializer block
-        assertFileMatches(fileName, quote("{") + "\\b+" +
-                R_INC + quote("switch (kkk) {") + R_CASE_EXPRESSION_LEFT +
-                quote("") + R_CASE_EXPRESSION_RIGHT)
-
-        // static block
-        assertFileMatches(fileName, quote("") + R_CASE_EXPRESSION_LEFT +
-                quote("") + R_CASE_EXPRESSION_RIGHT)
+        assertFileMatches(fileName,
+                R_INC + quote("switch (kkk) {") + "\\s+" +
+                quote("case 88 ->") + R_CASE_EXPRESSION_LEFT + quote(" System.out.println(\"88\")") + R_CASE_EXPRESSION_RIGHT + ";" + "\\s+" +
+                quote("default ->") + R_CASE_EXPRESSION_LEFT + quote(" System.out.println(\"not 88\")") + R_CASE_EXPRESSION_RIGHT + ";")
     }
 
-    @Test
-    void testSwitchCaseWithColonsMixedWithExpressions() {
-        assumeTrue(JavaEnvUtils.isAtLeastJavaVersion(JavaEnvUtils.JAVA_14))
-
-        final String fileName = "Java14SwitchExpressionMixedCasesFailed.java"
-        final File srcFile = new File(srcDir, fileName)
-        int returnCode = instrumentSourceFileNoAssert(srcFile, JavaEnvUtils.JAVA_14, [] as String[])
-
-        // it's not allowed to mix "case X:" with "case X ->" in the same switch statement
-        assertEquals(1, returnCode)
-    }
 }
